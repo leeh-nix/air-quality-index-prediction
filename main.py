ec513387bb57bd85d7f8953a8f9bea7c97e13e5c
kokose <nkso1419@gmail.com>
2024-01-28 14:16:40 +0530

    one hot encoded categorical columns

--- main.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-import tensorflow as tf
-import pathlib
-from sklearn.model_selection import train_test_split
-
-dataset_path = pathlib.Path("/content/drive/MyDrive/")
-df = pd.read_csv(f"{dataset_path}/weather_dataset.csv")
-dataset = df
